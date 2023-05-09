d48750ad1b9a8837bfc39f8a5e1c3aa705ab8bf4
Phanuphong Machai <phanuphong.forwork@gmail.com>
2023-05-09 21:18:45 +0700

    feat: skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import { DefaultLayout } from "@/components/layouts/default.layout";
-import { Container, Text, Button, Icon } from "@chakra-ui/react";
+import {
+  Container,
+  Text,
+  Button,
+  Icon,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
 import { useFetchData } from "@/hook/useFetchData";
 import { quoteConfig } from "@/configs/quote";
 import { QuoteType } from "@/types/quote.type";
@@ -8,6 +15,7 @@ import { useToast } from "@chakra-ui/react";
 import { RWebShare } from "react-web-share";
 import { FiCopy, FiShare2 } from "react-icons/fi";
 import Head from "next/head";
+import classNames from "classnames";
 
 export default function Home() {
   const toast = useToast();
@@ -37,20 +45,36 @@ export default function Home() {
         <meta property="og:image" content="/images/landing.png" />
       </Head>
       <DefaultLayout>
+        {quotes.isLoading && (
+          <Container>
+            <SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="2" />
+            <Skeleton height="4px" mt={12} />
+            <div className="flex justify-center mt-12 gap-8">
+              <Skeleton height="56px" rounded={"lg"} width={"100px"} />{" "}
+              <Skeleton height="56px" rounded={"lg"} width={"100px"} />
+            </div>
+          </Container>
+        )}
+
         {quotes.data?.map((quote, index) => {
           return (
             <>
-              <Container key={index} className="p-8 md:p-0">
-                <Text as={"i"} fontSize={{ base: "xl", md: "3xl", lg: "5xl" }}>
-                  {"“ "} {quote?.quote} {" ”"}
-                </Text>
-                <Text mt={12}>
-                  By{" "}
-                  <span className="text-md lg:text-xl font-semibold">
-                    {quote?.author}
-                  </span>
-                </Text>
-              </Container>
+              {!quotes.isLoading && (
+                <Container key={index} className="p-8 md:p-0">
+                  <Text
+                    as={"i"}
+                    fontSize={{ base: "xl", md: "3xl", lg: "5xl" }}
+                  >
+                    {"“ "} {quote?.quote} {" ”"}
+                  </Text>
+                  <Text mt={12}>
+                    By{" "}
+                    <span className="text-md lg:text-xl font-semibold">
+                      {quote?.author}
+                    </span>
+                  </Text>
+                </Container>
+              )}
 
               <Container mt={12}>
                 <div className="w-full flex gap-4 justify-center">
